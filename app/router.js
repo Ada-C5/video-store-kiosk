265d63c73089efceec641a6a75506e65872e51b9
Anna Wilson <annie.marie.mason@gmail.com>
2016-07-27 16:25:42 -0700

    clean out code

--- app/router.js
+++ app/router.js
@@ -6,19 +6,19 @@ const Router = Ember.Router.extend({
   rootURL: config.rootURL
 });
 
-Router.map(function() {
-  this.route('movies', { resetNamespace: true }, function(){}
-    // {
-    //   this.route('movie', { resetNamespace: true, path: '/:movie_id' });
-    // }
-  );
-});
-
-// export default Router.map(function() {
-//   this.route('movies', { resetNamespace: true }, function() {
-//     this.route('movie', { resetNamespace: true, path: '/:movie_id' });
-//   });
+// Router.map(function() {
+//   this.route('movies', { resetNamespace: true }, function(){}
+//     {
+//       this.route('movie', { resetNamespace: true, path: '/:movie_id' });
+//     }
+//   );
 // });
 
+export default Router.map(function() {
+  this.route('movies', { resetNamespace: true }, function() {
+    this.route('movie', { resetNamespace: true, path: '/:movie_id' });
+  });
+});
 
-export default Router;
+
+// export default Router;

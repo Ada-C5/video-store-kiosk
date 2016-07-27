fb1e2f6885d26f0badedc11e47b0e0423f5bf1eb
Adriana <dri19tcc@gmail.com>
2016-07-27 15:30:09 -0700

    added an adapter route

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,7 +2,5 @@ import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
   // namespace: 'api/v1',
-  host: 'http://localhost:3000/'
+  host: 'http://localhost:3000'
 });
-
-// export default DS.ActiveModelAdapter.extend({
--- app/router.js
+++ app/router.js
@@ -8,6 +8,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('movies');
+  this.route('movie');
 });
 
 export default Router;

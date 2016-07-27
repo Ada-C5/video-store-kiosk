8c20354309d55122bf3e88069bffa12625e0bd1c
Anna Wilson <annie.marie.mason@gmail.com>
2016-07-27 16:41:01 -0700

    cleaned up commented out code

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,9 +1,3 @@
-// import DS from 'ember-data';
-//
-// export default DS.RESTAdapter.extend({
-//   // namespace: 'api/v1',
-//   host: 'http://localhost:3000'
-// });
 import DS from 'ember-data';
 
 export default DS.Adapter.extend({
@@ -14,7 +8,7 @@ export default DS.Adapter.extend({
       Ember.$.getJSON(url, query).then(function(data) {
         Ember.run(null, resolve, data);
       }, function(jqXHR) {
-        jqXHR.then = null; // tame jQuery's ill mannered promises
+        jqXHR.then = null;
         Ember.run(null, reject, jqXHR);
       });
     });

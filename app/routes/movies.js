a37a4d0af8f386bd48764e20acc2472d3bb9db84
Adriana <dri19tcc@gmail.com>
2016-07-27 14:24:00 -0700

    Added fake data to work on, templates for them and a way to view them

--- app/routes/movies.js
+++ app/routes/movies.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return ['The Notebook', 'Idiocracy', 'Old School', 'Jaws', 'Office Space', 'Fight Club', 'Alien', 'Legally Blonde', 'Toy Story', 'Finding Nemo', 'Forrest Gump', 'Fargo', 'Finding Dory', 'Zootopia'];
+  }
 });

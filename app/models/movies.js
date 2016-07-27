ea057276f9941a81d15f6ab07b328df0b8e52642
Jade Vance <jadenicolevance@gmail.com>
2016-07-27 16:58:14 -0700

    Adapter, controller, model, route and all of the things. Can get data from the api now

---
+++ app/models/movies.js
@@ -0,0 +1,10 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  // id: DS.attr('integer'),
+  title: DS.attr('string')
+  // overview: DS.attr('string'),
+  // releaseDate: DS.attr('date'),
+  // inventory: DS.attr('integer'),
+  // posterFilename: DS.attr('string')
+});

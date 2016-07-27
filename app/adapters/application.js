f0d1f7b9870c25f79ddb244ccad9117fec38cd15
Anna Wilson <annie.marie.mason@gmail.com>
2016-07-27 16:35:04 -0700

    api is connected to app.... wow

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,22 @@
+// import DS from 'ember-data';
+//
+// export default DS.RESTAdapter.extend({
+//   // namespace: 'api/v1',
+//   host: 'http://localhost:3000'
+// });
 import DS from 'ember-data';
 
-export default DS.RESTAdapter.extend({
-  // namespace: 'api/v1',
-  host: 'http://localhost:3000'
+export default DS.Adapter.extend({
+  findAll: function(store, type, sinceToken) {
+    var url = "http://localhost:3000";
+    var query = { since: sinceToken };
+    return new Ember.RSVP.Promise(function(resolve, reject) {
+      Ember.$.getJSON(url, query).then(function(data) {
+        Ember.run(null, resolve, data);
+      }, function(jqXHR) {
+        jqXHR.then = null; // tame jQuery's ill mannered promises
+        Ember.run(null, reject, jqXHR);
+      });
+    });
+  }
 });

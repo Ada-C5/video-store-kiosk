7f427e48a63c188bab4f6f7960f2e91a7ba90671
Adriana <dri19tcc@gmail.com>
2016-07-27 21:36:42 -0700

    Took out extraneous data

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,11 +2,11 @@ import DS from 'ember-data';
 
 export default DS.Adapter.extend({
   host: 'http://localhost:3000',
-  findAll: function(store, type, sinceToken) {
+  findAll: function() {
     var url = this.host;
-    var query = { since: sinceToken };
+    // var query = { since: sinceToken };
     return new Ember.RSVP.Promise(function(resolve, reject) {
-      Ember.$.getJSON(url, query).then(function(data) {
+      Ember.$.getJSON(url).then(function(data) {
         Ember.run(null, resolve, data);
       }, function(jqXHR) {
         jqXHR.then = null; // tame jQuery's ill mannered promises

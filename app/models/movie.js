69a978772acf493aec125742e05732f7db15d70d
Adriana <dri19tcc@gmail.com>
2016-07-28 10:21:53 -0700

    Added styling to the view and cleaned up comments

--- app/models/movie.js
+++ app/models/movie.js
@@ -1,11 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  // id: DS.attr('number'),
   title: DS.attr('string'),
   overview: DS.attr('string'),
   release_date: DS.attr('string'),
   inventory: DS.attr('number'),
   poster_filename: DS.attr('string'),
-
 });
--- app/router.js
+++ app/router.js
@@ -7,7 +7,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('movies');
+  // this.route('movies');
   this.route('movie');
 });
 

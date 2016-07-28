aa18278db463b1539dc66715fdd676187c5e6244
Adriana <dri19tcc@gmail.com>
2016-07-27 22:03:42 -0700

    Testing out page pagination

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -4,9 +4,9 @@ export default DS.Adapter.extend({
   host: 'http://localhost:3000',
   findAll: function() {
     var url = this.host;
-    // var query = { since: sinceToken };
+    var query = { size: 10 };
     return new Ember.RSVP.Promise(function(resolve, reject) {
-      Ember.$.getJSON(url).then(function(data) {
+      Ember.$.getJSON(url, query).then(function(data) {
         Ember.run(null, resolve, data);
       }, function(jqXHR) {
         jqXHR.then = null; // tame jQuery's ill mannered promises

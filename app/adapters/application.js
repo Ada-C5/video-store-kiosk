064f5444babf924a252996c331b682cf6ed7be70
Jade Vance <jadenicolevance@gmail.com>
2016-07-28 14:29:02 -0700

    modified the adapter to make it clearer where page numbers get passed in

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,10 +2,13 @@ import DS from 'ember-data';
 
 export default DS.Adapter.extend({
 
-  query: function(store, type, query) {
-    var url = "http://localhost:3000/";
+  query: function(store, type, page) {
+    console.log('hey bro you hit the adapter')
+    var url = "http://localhost:3000";
+    // mental note to turn page into a variable
+    var page = {page: 2}
     return new Ember.RSVP.Promise(function(resolve, reject) {
-      Ember.$.getJSON(url, query).then(function(data) {
+      Ember.$.getJSON(url, page).then(function(data) {
         Ember.run(null, resolve, data);
       }, function(jqXHR) {
         jqXHR.then = null; // tame jQuery's ill mannered promises

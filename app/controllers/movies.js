7326e031258c3dcfd1cf436da855bd8f233a6617
Lisa Rolczynski <lisa.sano@gmail.com>
2016-07-28 13:50:27 -0700

    add movies controller

---
+++ app/controllers/movies.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  queryParams: ['page', 'size'],
+  page: 1,
+  size: 10,
+  actions: {
+    nextPage(){
+      if (this.page < 10) {
+        this.set('page', this.page + 1);
+      }
+    },
+    prevPage(){
+      if (this.page > 1) {
+        this.set('page', this.page - 1)
+      }
+    }
+  }
+});

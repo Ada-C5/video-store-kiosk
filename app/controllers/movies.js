886925f08ec2daab5a2d81a5206da1800d4f53e7
Sarah <sarahkane682@yahoo.com>
2016-07-28 14:15:13 -0700

    page traverse is working

--- app/controllers/movies.js
+++ app/controllers/movies.js
@@ -1,16 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  page: 1,
+
   ajax: Ember.inject.service(),
   actions: {
-    netxTen() {
-      return this.get('ajax').request('http://localhost:3000/?page=3&size=10', {
+    nextTen() {
+    let nextPage = this.page + 1
+    this.set('page', nextPage)
+    },
+
+    previousTen() {
+      return this.get('ajax').request('http://localhost:3000/?page=1&size=10', {
         method: 'GET',
         data: {
 
         }
-      });
+      }).then(function(results){this.set('model', results)}.bind(this))
     }
+
+
+
+
   }
 });
 
--- app/routes/movies.js
+++ app/routes/movies.js
@@ -1,8 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
   ajax: Ember.inject.service(),
-  model() {
-    return this.get('ajax').request('http://localhost:3000/');
+  model(params) {
+    let page = params.page
+    let pageNumber = page || 1
+    return this.get('ajax').request('http://localhost:3000/?page=' + pageNumber + '&size=10');
+  },
+
+  queryParams:{
+    page:{
+      refreshModel: true
+    }
   }
 });

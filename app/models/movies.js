32b71fc2c5e2105d40eb1f751a66b0c8ee5cbf4a
Jade Vance <jadenicolevance@gmail.com>
2016-07-28 14:30:44 -0700

    pictures now work, yaaay

--- app/models/movies.js
+++ app/models/movies.js
@@ -1,10 +1,10 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  // id: DS.attr('integer'),
+  // id: DS.attr('number'),
   title: DS.attr('string'),
-  // overview: DS.attr('string'),
-  // releaseDate: DS.attr('date'),
-  // inventory: DS.attr('integer'),
-  posterFilename: DS.attr('string')
-});
+  release_date: DS.attr('date'),
+  overview: DS.attr('string'),
+  inventory: DS.attr('number'),
+  poster_filename: DS.attr('string')
+});s

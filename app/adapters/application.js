8caac5e09f2604835592bff086c95c777af41943
Deirdre Storck <deirdre.storck@gmail.com>
2016-07-28 09:34:22 -0700

    did some css to get posters displaying nicely

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,11 +1,11 @@
-import DS from 'ember-data';
-
-export default DS.ActiveModelAdapter.extend({
-  namespace: 'api/v1',
-  host: 'https://localhost:3000/'
-});
-
-
-App.ApplicationAdapter = DS.RESTAdapter.extend({
-  host: 'https://localhost:3000'
-});
+// import DS from 'ember-data';
+//
+// export default DS.ActiveModelAdapter.extend({
+//   namespace: 'api/v1',
+//   host: 'https://localhost:3000/'
+// });
+//
+//
+// App.ApplicationAdapter = DS.RESTAdapter.extend({
+//   host: 'https://localhost:3000'
+// });

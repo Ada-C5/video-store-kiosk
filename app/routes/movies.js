825daaee0ffa5efff67afa7e6ee655a026b1f643
Jessica <jessica.weeber@gmail.com>
2016-07-28 09:30:26 -0700

    add images to template

--- app/routes/movies.js
+++ app/routes/movies.js
@@ -2,11 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model(){
-    // return ["Alien", "Jaws", "Speed", "Finding Nemo", "Star Wars", "Finding Dory"]
-
-    return this.store.findAll('movie')
-    // return this.store.find('movie', "Jaws")
-  }
+      return this.store.findAll('movie')
+    }
 });
 
 

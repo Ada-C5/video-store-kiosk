f8e6dc46c55337f4d4a0de8907bdbe074f6fc13a
Jessica <jessica.weeber@gmail.com>
2016-07-29 13:19:29 -0700

    not getting much done because css and html in ember are not working. buttons are styled the same even though they are not showing up like that on the page. ember is adding html tags that do not exist

--- app/routes/movies.js
+++ app/routes/movies.js
@@ -2,8 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
    model: function() {
-   var query = { page: this.controllerFor('movies').get('page') }
-   return this.store.query('movie', query)
+   var query = { page: this.controllerFor('movies').get('page') };
+   return this.store.query('movie', query);
   },
 
   queryParams: {

c09abfa7349f91aa1d5d286761e0b7040934c6b2
Cristal Tay <bluecolorpencils@gmail.com>
2016-07-29 15:59:01 -0700

    just css'ed meh. time to queue it up

--- app/controllers/movies.js
+++ app/controllers/movies.js
@@ -5,7 +5,24 @@ export default Ember.Controller.extend({
   queryParams: ['page', 'size'],
   page: 1,
   size: 10,
+  queue: [],
   actions: {
+    select(title, overview, img){
+      var modal = document.getElementById('myModal');
+      // var modal = document.getElementsByClassName('modal-content')
+      console.log("title", modal)
+      this.set('displayTitle', title)
+      this.set('displayOverview', overview)
+      this.set('displayImg', img)
+      modal.style.display = "block"
+    },
+    close(){
+      var modal = document.getElementById('myModal');
+      modal.style.display = "none"
+    },
+    queue(movie){
+
+    },
     prev() {
       if (this.page !== 1) {
         this.set('page', this.page-1)

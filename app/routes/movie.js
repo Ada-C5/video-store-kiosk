da467a05f95895f1f81b7243ea2a3678ff0bfe06
Adriana <dri19tcc@gmail.com>
2016-07-29 11:43:57 -0700

    Cleaning up code and working on pagination

--- app/routes/movie.js
+++ app/routes/movie.js
@@ -1,15 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function(params) {
-    var query = { page: params.page };
-    return this.store.query('movie', query);
-  },
   page: 1,
   size: 10,
   queryParams: {
     page: {
       refreshModel: true
     },
-  }
+  },
+  model: function(params) {
+    var query = { page: params.page };
+    return this.store.query('movie', query);
+  },
 });

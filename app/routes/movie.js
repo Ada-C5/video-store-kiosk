859843b053bd36431813e73544a2e923ee3aa12d
Anna Wilson <annie.marie.mason@gmail.com>
2016-07-29 13:51:59 -0700

    movie show page adds correct movie

--- app/routes/movie.js
+++ app/routes/movie.js
@@ -1,8 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function() {
-    var id = "Jaws"
+  model: function(params) {
+    var id = params.movie_title
     return this.store.findRecord('movies', id)
   },
 

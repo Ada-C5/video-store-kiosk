b6b044acf2836d92cba132fe36b61c95c63a61cd
Valerie <val.conklin@gmail.com>
2016-07-29 13:51:25 -0700

    Can show movie title for individual movie on show template

---
+++ app/routes/show.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+
+  model: function(params) {
+    var title = params.title
+    return this.store.findRecord('movie', title)
+  }
+
+
+  // queryParams: {
+  //   page: {
+  //     refreshModel: true
+  //   },
+  //
+  //   title: {
+  //     refreshModel: true
+  //   }
+  // }
+});

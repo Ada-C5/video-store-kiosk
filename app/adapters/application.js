b0e96658fd590735eb14719011e3316ebf51ff32
Anna Wilson <annie.marie.mason@gmail.com>
2016-07-29 13:21:57 -0700

    every single movie page makes a request for jaws... need to update so it'll be movie specific and not just jaws all day every day

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -12,6 +12,19 @@ export default DS.Adapter.extend({
         Ember.run(null, reject, jqXHR);
       });
     });
-  }
+  },
+
+  findRecord: function(store, type, id, snapshot) {
+   var url = ["http://localhost:3000/title", id].join('/');
+
+   return new Ember.RSVP.Promise(function(resolve, reject) {
+     Ember.$.getJSON(url).then(function(data) {
+       Ember.run(null, resolve, data);
+     }, function(jqXHR) {
+       jqXHR.then = null; // tame jQuery's ill mannered promises
+       Ember.run(null, reject, jqXHR);
+     });
+   });
+ }
 
 });
--- app/router.js
+++ app/router.js
@@ -9,4 +9,5 @@ const Router = Ember.Router.extend({
 export default Router.map(function() {
   this.resource('movies')
   this.resource('movie', { path: 'movies/:movie_title' });
-  });
+  // this.route('movie');
+});

63ca06bdd711aed56c0968f05e5b7f06006ef983
Adriana <dri19tcc@gmail.com>
2016-07-31 12:57:58 -0700

    Adding a modal to each movie

--- app/video-kiosk.js
+++ app/video-kiosk.js
@@ -1,1 +1,14 @@
-//put ajax stuff in here!
+//put ajax(ember handled ajax for me) and js stuff in here!
+import Ember from 'ember';
+
+var $ = Ember.$;
+
+$(function() { //Shorthand for document.ready
+  $(document).on('click', '.movie-poster', function() {
+    $(this).siblings('.modal').modal();
+  })
+  $('.movie-poster').on('click', function() {
+    $(this).siblings('.modal').modal();
+  })
+
+})

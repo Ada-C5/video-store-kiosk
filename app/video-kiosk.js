e4ae2531d2ae59f3601b87581d65677068781ac5
Adriana <dri19tcc@gmail.com>
2016-07-31 13:44:37 -0700

    Trying to get elements

--- app/video-kiosk.js
+++ app/video-kiosk.js
@@ -1,14 +1,21 @@
 //put ajax(ember handled ajax for me) and js stuff in here!
 import Ember from 'ember';
 
-var $ = Ember.$;
+// var $ = Ember.$;
 
-$(function() { //Shorthand for document.ready
-  $(document).on('click', '.movie-poster', function() {
-    $(this).siblings('.modal').modal();
-  })
-  $('.movie-poster').on('click', function() {
-    $(this).siblings('.modal').modal();
-  })
+// $(function() { //Shorthand for document.ready
+//   $(document).on('click', '.movie-poster', function() {
+//     console.log("I'm clicking poster!");
+//     $(this).siblings('.modal').modal();
+//   });
+// });
 
-})
+// App = Ember.Application.create({
+//   ready: function() {
+//     // will be called when the app is ready
+//     $(document).on('click', '.movie-poster', function() {
+//       console.log("I'm clicking poster!");
+//       $(this).siblings('.modal').modal();
+//     });
+//   }
+// })

e6b8231d2d0c5cb470f72926ae337677f78c5cb6
Anna Wilson <annie.marie.mason@gmail.com>
2016-08-01 10:58:59 -0700

    using jquery from the movie controller to empty outlet div... this might not be the best way...

--- app/controllers/movie.js
+++ app/controllers/movie.js
@@ -3,7 +3,11 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     unselect() {
-      console.log("I worked")
-    }
+      console.log("Clear Outlet!")
+      console.log($('.selectedmovie').empty());
+    },
+    queue() {
+      console.log("Add to Queue, Clear Outlet!")
+    },
   }
 });

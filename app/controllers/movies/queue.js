a5bcfe4114ffb0d0f53d255edddfa482c064c008
Leah <leahnpetersen@gmail.com>
2016-08-01 11:07:35 -0700

    added button to clear queue

--- app/controllers/movies/queue.js
+++ app/controllers/movies/queue.js
@@ -5,21 +5,32 @@ var { computed, get } = Ember;
 export default Ember.Controller.extend({
 
 	selectedMovies        : Ember.A([]),
-  remainingMovies       : computed.setDiff('model', 'selectedMovies'),
-  remainingMoviesLength : computed.alias('remainingMovies.length'),
+  // remainingMovies       : computed.setDiff('model', 'selectedMovies'),
+  // remainingMoviesLength : computed.alias('remainingMovies.length'),
+
+//   clearQueue: Ember.computed('selectedMovies', function () {
+//   return this.get('selectedMovies'):Ember.A([]);
+// })
 
 	actions: {
     addMovie(id) {
-      console.log("HI")
       var selectedMovies = get(this, 'selectedMovies');
-      console.log(id)
+      // console.log(id)
       var movie          = this.store.findRecord('movies', parseInt(id));
-      console.log(movie.id)
+      // console.log(movie.id)
 
       if (!selectedMovies.contains(movie)) {
         selectedMovies.pushObject(movie);
       }
-      console.log(selectedMovies)
     },
+
+    clearQueue() {
+      console.log("um?")
+      var selectedMovies = get(this, 'selectedMovies');
+      selectedMovies.clear();
+      console.log(selectedMovies)
+    }
+
+
   }
 });

21e18e78c62c08492891b1cddcdbe32713719d42
Anna Wilson <annie.marie.mason@gmail.com>
2016-08-01 14:15:36 -0700

    remove unneccesary jquery hiding of div

--- app/routes/movie.js
+++ app/routes/movie.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    ($('.selectedmovie').show());
     var id = params.movie_title
     return this.store.findRecord('movies', id)
   }

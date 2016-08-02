3d48c105f30e4380d30e328dd51075dec7383b14
Nadine Curry <nlcurry@gmail.com>
2016-08-02 10:08:10 -0700

    set model data

--- app/models/movie.js
+++ app/models/movie.js
@@ -1,5 +1,10 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  title: DS.attr('string'),
+  overview: DS.attr('string'),
+  release_date: DS.attr('string'),
+  inventory: DS.attr('number'),
+  poster_filename: DS.attr('string')
 
 });

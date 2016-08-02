51ba301338bcd57d08357ee3e28a9986b45df9b6
Adriana <dri19tcc@gmail.com>
2016-08-02 09:50:30 -0700

    Formatting Modal

--- app/components/movie-modal.js
+++ app/components/movie-modal.js
@@ -5,13 +5,7 @@ export default Ember.Component.extend({
   actions: {
     ok: function() {
       this.$('.modal').modal('hide');
-      console.log("you're in the component");
       this.sendAction('addQueue'); //This should add to queue
     }
   }
-  // show: function() {
-  //   this.$('.modal').modal().on('hidden.bs.modal', function() {
-  //     this.sendAction('close');
-  //   }.bind(this));
-  // }.on('didInsertElement')
 });
--- app/controllers/movie.js
+++ app/controllers/movie.js
@@ -5,7 +5,7 @@ export default Ember.Controller.extend({
   page: 1,
   size: 10,
   queue: [],
-  modalMovie: "", // view is not updating
+  modalMovie: "",
 
   actions: {
     nextPage() {

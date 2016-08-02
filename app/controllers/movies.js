147abd1b040b3fe29a75c0782aaac4b3de36d7dc
Nadine Curry <nlcurry@gmail.com>
2016-08-02 11:03:07 -0700

    setting page and size with actions

--- app/controllers/movies.js
+++ app/controllers/movies.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  queryParams: ['page', 'size'],
+  page: 1,
+  size: 10,
+  actions: {
+    nextPage: function() {
+      if (this.page <10) {
+        this.incrementProperty('page'),
+        this.set('page')
+      }
+    },
+
+    previousPage: function() {
+      if (this.page <10) {
+        this.decrementProperty('page'),
+        this.set('page')
+      }
+    }
+  }
 });

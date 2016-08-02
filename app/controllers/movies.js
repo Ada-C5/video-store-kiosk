79abcf4686e8b1b24655156e430a3ee3c720c9d8
Anna Wilson <annie.marie.mason@gmail.com>
2016-08-02 16:07:54 -0700

    button clears queue

--- app/controllers/movies.js
+++ app/controllers/movies.js
@@ -4,12 +4,17 @@ export default Ember.Controller.extend({
   page: 1,
 
   actions: {
+    deleteQueue() {
+      $('.navbar-text').empty()
+    },
+
     nextPage() {
       if (this.page + 1 <= 10) {
         this.transitionToRoute('movies');
         this.set("page", (this.page+1));
       }
     },
+    
     previousPage() {
       if (this.page - 1 >= 1) {
         this.transitionToRoute('movies');

cce255eda6d7a25067e437d7e4eb61e8f9e56502
Sarah <sarahkane682@yahoo.com>
2016-08-02 11:19:27 -0700

    queue almost working

--- app/controllers/movies.js
+++ app/controllers/movies.js
@@ -26,7 +26,7 @@ export default Ember.Controller.extend({
 
 
     enQueue(){
-    
+
 
     }
 
--- app/routes/movies.js
+++ app/routes/movies.js
@@ -4,10 +4,15 @@ export default Ember.Route.extend({
 
   ajax: Ember.inject.service(),
   model(params) {
-    
+
     let page = params.page
     let pageNumber = page || 1
-    return this.get('ajax').request('http://localhost:3000/?page=' + pageNumber + '&size=10');
+    return this.get('ajax').request('http://localhost:3000/?page=' + pageNumber + '&size=10')
+    .then((movies)=>{ var queue = this.controllerFor('movies').get('queue')
+        console.log(queue)
+      return {q: queue, m: movies}
+    })
+    .catch(function(error){console.log("failed")})
   },
 
   queryParams:{

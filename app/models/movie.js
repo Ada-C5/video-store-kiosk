c669f93d97bcc4dc32cc7356d26b2e6f4d7bfdc5
Nadine Curry <nlcurry@gmail.com>
2016-08-03 10:43:49 -0700

    changed field names so images will show up

--- app/models/movie.js
+++ app/models/movie.js
@@ -3,8 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   overview: DS.attr('string'),
-  release_date: DS.attr('string'),
+  release_date: DS.attr('date'),
   inventory: DS.attr('number'),
   poster_filename: DS.attr('string')
-
 });

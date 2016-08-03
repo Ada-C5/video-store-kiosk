d85cb49ee9fde9dab56e70ebca76003f19793a8a
Alysia Brown <alysiacbrown@gmail.com>
2016-08-03 11:10:57 -0700

    Added hover feature for displaying movie description and release date.

--- app/controllers/movies.js
+++ app/controllers/movies.js
@@ -2,20 +2,24 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
- page: 1,
- size: 10,
- actions: {
-   getNext(){
-     // console.log(this.get('page'))
-     this.set('page', (this.get('page')+1));
+  page: 1,
+  size: 10,
+  actions: {
+    getNext(){
+      this.set('page', (this.get('page')+1));
 
-   },
-   getPrev(){
-     if (this.get('page')>1) {
-       this.set('page', (this.get('page')-1));
-     } else {
-       console.log('first page!')
-     }
-   }
- }
+    },
+    getPrev(){
+      if (this.get('page')>1) {
+        this.set('page', (this.get('page')-1));
+      }
+    }
+    // getMovieInfo(){
+    //   var $this = this.$()
+    //   $this.hover(function() {
+    //     console.log("hello");
+    //   })
+    // }
+
+  }
 });

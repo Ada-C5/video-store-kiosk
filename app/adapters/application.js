ad90fadb213ce3cc40be549ba2c3c7e6e13e2c38
Anna Wilson <annie.marie.mason@gmail.com>
2016-08-04 10:41:30 -0700

    cleaning out comments, extraneous things

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -8,7 +8,7 @@ export default DS.Adapter.extend({
       Ember.$.getJSON(url, query).then(function(data) {
         Ember.run(null, resolve, data);
       }, function(jqXHR) {
-        jqXHR.then = null; // tame jQuery's ill mannered promises
+        jqXHR.then = null;
         Ember.run(null, reject, jqXHR);
       });
     });
@@ -21,7 +21,7 @@ export default DS.Adapter.extend({
      Ember.$.getJSON(url).then(function(data) {
        Ember.run(null, resolve, data);
      }, function(jqXHR) {
-       jqXHR.then = null; // tame jQuery's ill mannered promises
+       jqXHR.then = null; 
        Ember.run(null, reject, jqXHR);
      });
    });

25a4ed8523a90afe983658fe2f79b9cb3bc14c2c
Jade Vance <jadenicolevance@gmail.com>
2016-08-04 09:37:09 -0700

    cleanup of commented out code

--- app/router.js
+++ app/router.js
@@ -1,26 +1,3 @@
-// import Ember from 'ember';
-// import config from './config/environment';
-
-// const Router = Ember.Router.extend({
-//   location: config.locationType,
-//   rootURL: config.rootURL
-// });
-
-// Router.map(function() {
-//   this.resource('movies', function() {
-//     this.resource('movie-selected', { path: '/:movie_title'})
-//   })
-//   // this.route('movies');
-//   // this.route('page', {path: '/:page'}, function(){
-//     // this.route('queue')
-//   // })
-//   // this.route('movie-selected');
-// });
-
-// export default Router;
-
-// // dynamic routing with pages 
-
 import Ember from 'ember';
 import config from './config/environment';
 
